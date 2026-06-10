f1d36b75486073ccc748898c752df63fd1395bbe
hpuhr <compass@openats.at>
2026-06-10 09:48:57 +0200

    deploy fix

--- docker/deploy_jasterix.sh
+++ docker/deploy_jasterix.sh
@@ -1,19 +1,23 @@
 export ARCH=x86_64
+export APPIMAGE_EXTRACT_AND_RUN=1
 
 cd /app/workspace/jasterix/
-mkdir -p appimage/appdir/bin/
-cp /usr/bin/jasterix_client appimage/appdir/bin/
-mkdir -p appimage/appdir/lib/
-cp /usr/lib/libjasterix.a appimage/appdir/lib/
 
+# fresh AppDir - linuxdeploy populates usr/bin, usr/lib itself
+rm -rf appimage/appdir/*/
+mkdir -p appimage/appdir/
 
-if [[ $OS_NAME == "deb9" ]]
-then
-  /app/workspace/compass/docker/linuxdeployqt/linuxdeployqt-deb9-x86_64.AppImage --appimage-extract-and-run appimage/appdir/jasterix.desktop -appimage -bundle-non-qt-libs -verbose=2
-elif [[ $OS_NAME == "deb10" ]]
-then	
-/app/workspace/compass/docker/linuxdeployqt/linuxdeployqt-deb10-x86_64.AppImage --appimage-extract-and-run appimage/appdir/jasterix.desktop -appimage -bundle-non-qt-libs -verbose=2
-fi
+# build the AppImage with the same linuxdeploy used for COMPASS
+# (jASTERIX is a non-Qt CLI tool, so no qt/gtk plugins are needed)
+export OUTPUT=jASTERIX_client-x86_64.AppImage
+
+/app/workspace/compass/docker/linuxdeploy/linuxdeploy-x86_64.AppImage \
+    --appdir appimage/appdir \
+    --executable=/usr/bin/jasterix_client \
+    --desktop-file=appimage/appdir/jasterix.desktop \
+    --icon-file=appimage/appdir/atsdb.png \
+    --output appimage \
+    --verbosity=2
 
 cd definitions/
 zip -r ../jasterix_definitions.zip .
@@ -23,4 +27,3 @@ zip -r ../analyze.zip . -x ".*" -x "__*" -x "*/__*"
 
 
 cd /app/workspace/compass/docker
-
